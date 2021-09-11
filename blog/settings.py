281f391157b22a3282528f7b6f4d0e4f6b57f031
Vishnu S Reddy <vishnusreddy007@gmail.com>
2021-09-12 00:53:38 +0530

    Allowed Hosts Updated

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k6$(*(%m76)4dd%1%-y@eetftn8&ly%-+62#by%nh4petsxp$+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
